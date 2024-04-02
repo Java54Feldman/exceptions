c4ab647ad87658d4bf9983c6ad90960502103579
Pavel Feldman <feldmanpk@gmail.com>
2024-04-02 17:08:30 +0300

    HW #12 Exceptions & binary search algorithm

--- src/telran/exceptions/tests/BallBrokenFloorTest.java
+++ src/telran/exceptions/tests/BallBrokenFloorTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import telran.exceptions.BallBrokenFloor;
 
 class BallBrokenFloorTest {
-
 	private static final int N_FLOORS = 100;
 
 	@Test
@@ -15,13 +14,24 @@ class BallBrokenFloorTest {
 		BallBrokenFloor bbf = new BallBrokenFloor(N_FLOORS);
 		assertEquals(bbf.getBrokenFloor(), getMinBrokenFloor(bbf));
 	}
-
 	private int getMinBrokenFloor(BallBrokenFloor bbf) {
-		// TODO 
-		// using binary search algorithm and only method checkFloor 
+		// using binary search algorithm and only method checkFloor
 		// of the class BallBrokenFloor
 		// you should find minimal floor, from which a ball will be broken
-		return -1;
+		int left = 1;
+		int right = N_FLOORS;
+		int middle = (left + right) / 2;
+		int minBrokenFloor = 1;
+		while (left <= right) {
+			try {
+				bbf.checkFloor(middle);
+				left = middle + 1;
+			} catch (Exception e) {
+				minBrokenFloor = middle;
+				right = middle - 1;
+			}
+			middle = (left + right) / 2;
+		}
+		return minBrokenFloor;
 	}
-
 }
